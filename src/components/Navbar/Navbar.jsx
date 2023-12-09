618de5687341b5f778ee0756b92edc38a6c93466
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-09 23:43:53 +0530

    added navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbarWrapper">
+        <ul className="navbarList">
+          <li>Vegetables & Fruits</li>
+          <li>Dairy & Breakfast</li>
+          <li>Munchies</li>
+          <li>Cold Drinks & Juices</li>
+          <li>Instant & Frozen Food</li>
+          <li>Tea, Coffee & Health Drinks</li>
+          <li>Bakery & Biscuits</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
