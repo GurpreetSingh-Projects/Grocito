5fc666f3a6650efad523c5ce3e83aae8df8b5d72
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-10 02:34:35 +0530

    bug fixes

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,23 +1,15 @@
 import Shared from "../Shared/Shared";
 import "./feed.css";
+
 export default function Feed() {
   return (
     <div className="shared">
       <div className="sharedWrapper">
         <div className="sharedContainer">
           <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
-          <Shared />
+          {Posts.map((p) => (
+            <Post key={p.id} post={p} />
+          ))}
         </div>
       </div>
     </div>
