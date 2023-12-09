95b1b5c336fe891fda82506f19d209e2d99a12b8
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-10 02:34:21 +0530

    improved feed

---
+++ src/dummyData.js
@@ -0,0 +1,34 @@
+export const Items = [
+  {
+    id: 1,
+    photo:
+      "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/526764a.jpg?ts=1701686839",
+    userId: 1,
+    mp: 600,
+    sp: 540,
+  },
+  {
+    id: 2,
+    photo:
+      "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/526764a.jpg?ts=1701686839",
+    userId: 1,
+    mp: 600,
+    sp: 540,
+  },
+  {
+    id: 3,
+    photo:
+      "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/526764a.jpg?ts=1701686839",
+    userId: 1,
+    mp: 600,
+    sp: 540,
+  },
+  {
+    id: 4,
+    photo:
+      "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/526764a.jpg?ts=1701686839",
+    userId: 1,
+    mp: 600,
+    sp: 540,
+  },
+];
