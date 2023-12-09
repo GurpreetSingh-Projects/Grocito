f477803baf5ac3dd4b16d0462e89bc45544ef9ec
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-10 00:44:03 +0530

    added shared

---
+++ src/components/Shared/Shared.jsx
@@ -0,0 +1,20 @@
+import "./shared.css";
+export default function Shared() {
+  return (
+    <div className="shared">
+      <div className="sharedWrapper">
+        <div className="container">
+          <div className="sharedTop">
+            <img
+              className="itemImage"
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/526764a.jpg?ts=1701686839"
+              alt=""
+            />
+          </div>
+          <div className="sharedCenter"></div>
+          <div className="sharedBottom"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
