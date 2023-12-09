bae996ed1807f693704d95495ff32f2245742a98
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-10 05:00:55 +0530

    bug fixes

--- src/components/Shared/Shared.jsx
+++ src/components/Shared/Shared.jsx
@@ -1,11 +1,19 @@
 import "./shared.css";
+import { useState } from "react";
 export default function Shared(props) {
+  const [add, setAdd] = useState("Add");
+
   return (
     <div className="shared">
       <div className="sharedWrapper">
         <div className="container">
           <div className="sharedTop">
             <img className="itemImage" src={props.link} alt="" />
+            <img
+              className="discount"
+              src="https://ps.w.org/conditional-discount-rule-for-woocommerce/assets/icon.svg?rev=2540571"
+              alt=""
+            />
           </div>
           <div className="sharedCenter">
             <div className="sharedTitle">{props.name}</div>
@@ -14,7 +22,13 @@ export default function Shared(props) {
           <div className="sharedBottom">
             <div className="sharedSp">{props.sp}</div>
             <div className="sharedMp">{props.mrp}</div>
-            <button>ADD</button>
+            <button
+              onClick={() => {
+                setAdd("Added");
+              }}
+            >
+              {add}
+            </button>
           </div>
         </div>
       </div>
