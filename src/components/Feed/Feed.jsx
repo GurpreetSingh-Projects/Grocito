5bb0a23a6b63c579b3fb95f3ff14b379fb637845
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-10 00:43:38 +0530

    bug fixes

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,25 @@
+import Shared from "../Shared/Shared";
+import "./feed.css";
+export default function Feed() {
+  return (
+    <div className="shared">
+      <div className="sharedWrapper">
+        <div className="sharedContainer">
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+          <Shared />
+        </div>
+      </div>
+    </div>
+  );
+}
