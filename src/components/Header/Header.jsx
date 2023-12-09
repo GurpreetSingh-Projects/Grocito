1647c3168c64998beefacd49fc40a47a3316b54a
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-09 21:48:39 +0530

    optimized header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,17 +14,28 @@ export default function Header() {
             <input
               type="text"
               className="searchInput"
-              placeholder="Search for Products"
+              placeholder='Search for "Atta"'
             />
+            <select className="selectLocation">
+              <option value="Default">Select Location</option>
+              <option value="Delhi">Delhi</option>
+              <option value="Punjab">Punjab</option>
+              <option value="Andhra Pradesh">Andhra Pradesh</option>
+              <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+              <option value="Assam">Assam</option>
+              <option value="Pune">Pune</option>
+              <option value="Uttar Pradesh">Uttar Pradesh</option>
+              <option value="Uttarakhand">Uttarakhand</option>
+              <option value="Mumbai">Mumbai</option>
+            </select>
           </div>
-          <button className="selectLocation">Select Location</button>
         </div>
         <div className="headerRight">
           <div className="cart">
-            <img className="cart" src={cart} alt="" />
+            <img className="cartIcon" src={cart} alt="" />
             <div className="cartItems">5</div>
           </div>
-          <button className="className">Login/Signup</button>
+          <button className="login">Login/Signup</button>
         </div>
       </div>
     </div>
